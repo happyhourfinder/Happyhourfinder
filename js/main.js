98095fd21b68c67568b197604901915088228c89
Zac <zackchig@yahoo.com>
2024-10-16 22:19:30 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+document.querySelector('button').addEventListener('click', () => {
+    const location = document.querySelector('input').value;
+    console.log(`Searching for happy hours in ${location}`);
+});
